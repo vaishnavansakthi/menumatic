c5ec450a96535f2615ec1d1563175d432844d0cd
Vaishnavan M <145651333+vaishnavansakthi@users.noreply.github.com>
2025-07-19 19:36:19 +0530

    feat: test

--- src/App.test.js
+++ src/App.test.js
@@ -3,8 +3,8 @@ import React from 'react'
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
-test('renders learn react link', () => {
+test('renders hello heading', () => {
   render(<App />)
-  const linkElement = screen.getByText(/learn react/i)
-  expect(linkElement).toBeInTheDocument()
+  const heading = screen.getByText(/hello/i)
+  expect(heading).toBeInTheDocument()
 })
